ddda08f2cfaaaf0883361ff3bbb51245879f5288
colehenke <colehenke2@gmail.com>
2024-10-26 16:49:10 -0500

    Init commit

---
+++ src/main/java/com/stats/clashStats/ClashStatsApplication.java
@@ -0,0 +1,13 @@
+package com.stats.clashStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stats/clashStats/ClashStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stats.clashStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClashStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
